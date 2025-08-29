bf61bca75ad44b0c00803f2a8a353a0061cee27a
ken kendagor <kenkendagor3@gmail.com>
2025-08-29 23:44:26 +0300

    Add files via upload

---
+++ inde.sql
@@ -0,0 +1,23 @@
+
+-- Assignment: Database Indexing and Optimization
+
+
+-- Question 1 🗑️
+-- Drop an index named IdxPhone from the customers table
+DROP INDEX IdxPhone ON customers;
+
+-- Question 2 👤
+-- Create a user named bob with password 'S$cu3r3!' restricted to localhost
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+-- Question 3 🔑
+-- Grant INSERT privilege to user bob on the salesDB database
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+
+-- Question 4 🔐
+-- Change password for user bob to 'P$55!23'
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
+
+
+-- End of Assignment 🚀
+
